f5c62a96040e12c4d08e8472f77c099005a5bbb1
chieh-eric <ericwang0621@gmail.com>
2022-03-21 00:43:38 +0800

    update

--- 程式/Close_pair.cpp
+++ 程式/Close_pair.cpp
@@ -13,13 +13,14 @@ float dist(Point p1, Point p2)
 {
     return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
-Point p[SIZE],p_sy[SIZE],p_sx[SIZE];
-float brutefore(Point p [] , int n, int low)
+Point p[SIZE], p_sy[SIZE], p_sx[SIZE];
+int compareY(const void *a, const void *b);
+float brutefore(Point p[], int n, int low)
 {
     float min = SIZE;
-    for (int i = low; i < low+n; i++)
+    for (int i = low; i < low + n; i++)
     {
-        for (int j = i + 1; j < low+n; ++j)
+        for (int j = i + 1; j < low + n; ++j)
         {
             if (dist(p[i], p[j]) < min)
             {
@@ -33,8 +34,9 @@ float min(int x, int y)
 {
     return (x < y) ? x : y;
 }
-float update(Point domain [], int size, float d)
+float update(Point domain[], int size, float d)
 {
+    qsort(domain, size, sizeof(Point), compareY);
     float min = d;
     for (int i = 0; i < size; i++)
     {
@@ -46,9 +48,10 @@ float update(Point domain [], int size, float d)
             }
         }
     }
+    cout << min << endl;
     return min;
 }
-float closetPair( Point p_x[], Point p_y[], int low, int heigh)
+float closetPair(Point p_x[], Point p_y[], int low, int heigh)
 {
     if (heigh - low + 1 <= 3)
     {
@@ -59,9 +62,9 @@ float closetPair( Point p_x[], Point p_y[], int low, int heigh)
     float dl = closetPair(p_x, p_y, low, mid);
     float dr = closetPair(p_x, p_y, mid + 1, heigh);
     float d = min(dl, dr);
-    Point domain[heigh-low];
-    int j=0;
-    for (int i = 0; i < heigh - low + 1; i++)
+    Point domain[heigh - low + 1];
+    int j = 0;
+    for (int i = 0; i <= heigh - low + 1; i++)
     {
         if (abs(p_x[i].x - midPoint.x) < d)
         {
@@ -91,19 +94,19 @@ int main()
     for (int i = 0; i < num; i++)
     {
         cin >> temp.x >> temp.y;
-        p[i]=temp;
+        p[i] = temp;
     }
     low = 0;
-    heigh = num-1;
+    heigh = num - 1;
     qsort(p, num, sizeof(Point), compareY);
     for (int i = 0; i < num; i++)
     {
-        p_sy[i]=p[i];
+        p_sy[i] = p[i];
     }
-    qsort(p, num, sizeof(Point), compareY);
+    qsort(p, num, sizeof(Point), compareX);
     for (int i = 0; i < num; i++)
     {
         p_sx[i] = p[i];
     }
-    printf("%.3f",closetPair(p_sx, p_sy, low, heigh));
+    printf("%.3f\n", closetPair(p_sx, p_sy, low, heigh));
 }
